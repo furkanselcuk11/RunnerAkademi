ec4788fdbf185a5602cd76a9579dfb02bcf14162
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-11-09 20:56:09 +0300

    +add: Week-4 Finish Anim and Obstacle design added

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -7,8 +7,17 @@ using UnityEngine.SceneManagement;
 public class CollectCoin : MonoBehaviour
 {
     public int score;
+    public int maxScore;
     public TextMeshProUGUI scoreTxt;
     public PlayerController playerController;
+    public GameObject player;
+    private Animator anim;
+    public GameObject finishPanel;
+    private void Start()
+    {
+        anim = player.GetComponent<Animator>();
+        finishPanel.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {       
         if (other.gameObject.CompareTag("Coin"))
@@ -19,8 +28,21 @@ public class CollectCoin : MonoBehaviour
         }
         else if (other.gameObject.CompareTag("End"))
         {
-            Debug.Log("Congrats...!");
-            playerController.runningSpeed = 0;            
+            //Congrats...
+            playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180f, transform.rotation.z, Space.Self);
+            finishPanel.SetActive(true);
+
+            if (score >= maxScore)
+            {
+                // You Win...!
+                anim.SetBool("Win", true);
+            }
+            else
+            {
+                // You Lose...!"
+                anim.SetBool("Lose", true);
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -35,5 +57,9 @@ public class CollectCoin : MonoBehaviour
     {
         score++;
         scoreTxt.text = "Score: " + score.ToString();
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Sahneyi yeniden baþlat
     }
 }
